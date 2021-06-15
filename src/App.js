d456fed7d3640e3127b806e9097d1afcddabd928
Devansh Kumar <59093441+Devanshkr@users.noreply.github.com>
2021-06-15 16:03:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Rank from './Components/Rank/Rank';
-import Particles from 'react-particles-js';  //done npm install react-particle-js
+import Particles from 'react-particles-js';
 import './App.css';
 import SignIn from './Components/SignIn/SignIn';
 import Register from './Components/Register/Register';
@@ -25,7 +25,7 @@ const initialStage = {
   input: '',
   imageUrl: '',
   box: {},
-  route: 'signIn', //keeps track the route of the pages.
+  route: 'signIn',
   isSignedIn: false,
   user: {
     id: '',
@@ -41,8 +41,7 @@ class App extends Component {
     super();
     this.state = initialStage;
   }
-
-  //for new users
+  
   loadUser = (data) =>{
     this.setState({user:{
       id: data.id,
@@ -52,25 +51,13 @@ class App extends Component {
       joined: data.joined
     }})
   }
-
-  //LINKING THE BACKEND
-  // componentDidMount(){
-  //   fetch('http://localhost:3000/')
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  // }
-
-  //calculation part of where we have to draw box. getting coordinates.
+  
   calculateFace = (data) => {
     const ClarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    // console.log(ClarifaiFace);
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
-    // console.log(width, height);
     return {
-      //below are the corner points of border of box.
-      //cal. starts from top 
       leftCol: ClarifaiFace.left_col * width,
       topRow: ClarifaiFace.top_row * height,
       rightCol: width - (ClarifaiFace.right_col * width),
@@ -78,68 +65,37 @@ class App extends Component {
     }
   }
 
-  //box
   displayFace = (box) => {
-    // console.log(box);
     this.setState({box: box});
   }
 
-  //changing the value of input variable to the value of user has inputted.
   onInputChange = (event) => {
-    // console.log(event.target.value);
     this.setState({
       input: event.target.value
     });
   }
 
-  // When submit button is clicked do the required job.
   onButtonSubmit = () => {
-    // console.log('click');
     this.setState({imageUrl: this.state.input});
-      // fetch('http://localhost:3000/imageUrl', {
-        //doing below instead of above because we are deploying on heroku now.
         fetch('https://ancient-taiga-01841.herokuapp.com/imageUrl', {
-        method: 'post',  //yaha se image api ko laare hai
+        method: 'post',
         headers: { 'Content-type': 'application/json' },
         body: JSON.stringify({
           input: this.state.input
         })
       })
       .then(resp => resp.json())
-    // app.models.predict().then();  -> API CALL
-    /*
-      .predict(
-        //"f76196b43bbd45c99b4f3cd8e8b40a8a", //this is the model id - related to the api of internet
-        Clarifai.FACE_DETECT_MODEL,    //COLOR_MODEL,   //github se uthaya ye - clarifai github models
-        this.state.input) //here comes the link which we will take from the user.
-        //and can;t put imageUrl here, gives error.
-      .then(
-        function (response) {
-          //the path of image coordinates.
-          console.log(response);
-        },
-        function (err) {
-          console.log('sed error');
-        }
-      );  */
       .then(resp => {
         if(resp) {
           fetch('https://ancient-taiga-01841.herokuapp.com/image', {
             method: 'put',
-            //header accepts an object
             headers: { 'Content-type': 'application/json' },
-            //over backend we gotta stringify 
             body: JSON.stringify({
               id: this.state.user.id
             })
           })
           .then(response => response.json())
           .then(count => {
-            //this was giving name undefined error because, it changes every variable.
-            // this.setState({user: {
-            //   entries: count
-            // }})
-            //do this instead of above
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
           .catch(console.log)
@@ -149,7 +105,6 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
-  //Go to another page 
   onRouteChange = (route) => {
     if(route === 'signOut'){
       this.setState(initialStage);
@@ -166,7 +121,6 @@ class App extends Component {
         <Particles params={ParticleOps} className='Particles' />
         <Navigation onRouteChange={this.onRouteChange}
                     isSignedIn={this.state.isSignedIn} />
-        {/* USING TERNARY OPERATOR: WRAP IT IN {} BCZ IT'S JSX. */}
         { this.state.route === 'home' ? 
         <div>
           <Logo />
